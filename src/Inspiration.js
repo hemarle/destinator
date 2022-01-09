99ba0e7541e965b47d5392dffeed28107bf93ca3
Daramola Femi <dfmopeoluwa@gmail.com>
2022-01-09 19:00:15 +0000

    inspiration  mobile done

--- src/Inspiration.js
+++ src/Inspiration.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import './Inspiration.css'
+import img1 from './image/Rectangle6.svg'
+import img2 from './image/Rectangle7.svg'
+import img3 from './image/Rectangle8.svg'
+import img4 from './image/Rectangle9.svg'
 function Inspiration() {
     return (
         <div className='inspiration'>
             <h2>Inspiration for your next trip</h2>
             <div className="inspiration__Cards">
 
-            <div className="inspiration__Cards">
-<img src="" alt="" />
+            <div className="inspiration__Card">
+<img src={img1} alt="" />
             </div>
-            <div className="inspiration__Cards">
-<img src="" alt="" />
+            <div className="inspiration__Card">
+<img src={img2} alt="" />
             </div>
-            <div className="inspiration__Cards">
-<img src="" alt="" />
+            <div className="inspiration__Card">
+<img src={img3} alt="" />
             </div>
-            <div className="inspiration__Cards">
-<img src="" alt="" />
+            <div className="inspiration__Card">
+<img src={img4} alt="" />
             </div>
             </div>
         </div>
