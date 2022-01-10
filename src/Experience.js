a129434a73e63554b2aa9e108280ca8b94ca37d4
Daramola Femi <dfmopeoluwa@gmail.com>
2022-01-10 16:19:00 +0000

    mobile 90% done

--- src/Experience.js
+++ src/Experience.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./Experience.css";
+import img1 from "./image/things1.svg";
+import img2 from "./image/things2.svg";
 function Experience() {
   return (
     <div className="experience">
+        <h2>Discover AirBnB Experience</h2>
       <div className="experience__Cards">
         <div className="experience__Card">
-          <h3>Things to do on your trip</h3>
-          <a href="#"> Experiences</a>
+          <img src={img1} />
+          <div className="experience__CardDetails">
+            <h3>Things to do on your trip</h3>
+            <a href="#" className="button"> Experiences</a>
+          </div>
         </div>
         <div className="experience__Card">
-          <h3>Things to do on your trip</h3>
-          <a href="#"> Experiences</a>
+          <img src={img2} />
+          <div className="experience__CardDetails">
+            <h3>Things to do on your trip</h3>
+            <a href="#" className="button"> Experiences</a>
+          </div>
         </div>
       </div>
     </div>
