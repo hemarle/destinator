02d9b9e05602e55a1440de846b4b8b639c0561b8
Daramola Femi <dfmopeoluwa@gmail.com>
2022-01-10 17:28:48 +0000

    mobile and desktop done

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import './Footer.css'
+import facebookIcon from './image/facebook.svg'
+import twitterIcon from './image/twitter.svg'
+import instagramIcon from './image/instagram.svg'
 function Footer() {
     return (
         <div className='footer'>
         <div className='footer__Links'>
          
             <ul className="foooter__Support">
-                <li></li>
+                <li  className='title'>Support</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
             </ul>
             <ul className="foooter__Community">
-
+            <li className='title'>Community</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
             </ul>
             <ul className="foooter__Hosting">
-
+            <li className='title'>Hosting</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
             </ul>
             <ul className="foooter__About">
-
+            <li className='title'>About</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
+                <li>Lorem ipsum dolor</li>
             </ul>
         </div>
 
@@ -24,9 +43,9 @@ function Footer() {
                 2021 Airbnb Inc
             </div>
             <div className="footer__SocialRight">
-<img src="" alt="" />
-<img src="" alt="" />
-<img src="" alt="" />
+<img src={facebookIcon} alt="" />
+<img src={twitterIcon} alt="" />
+<img src={instagramIcon} alt="" />
             </div>
         </div>
         </div>
